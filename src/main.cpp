65318e4e10c18d4b26c781a4bbde99f4ade4fa7b
Menna Jaheen <menna.jaheen16@gmail.com>
2025-04-20 17:13:24 +0200

    light source can be dragged

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,75 @@
+#include <iostream>
+#include <fstream>
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include<iostream>
+using namespace std;
 
-int main()
-{
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+int main() {
+    const int width = 1280, height = 760;
+    sf::RenderWindow window(sf::VideoMode(width, height), "Ray Tracer");
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-            {
+    // Create image -> texture -> sprite
+    sf::Image image;
+    image.create(width, height, sf::Color::Black);
+
+    sf::Texture texture;
+    texture.loadFromImage(image);
+
+    sf::Sprite sprite(texture);
+
+    // Create a circle
+    sf::CircleShape circle(40);
+    circle.setFillColor(sf::Color::White);
+    circle.setOrigin({-30, -60});
+
+    sf::Vector2f dragOffset ;
+    bool isDragging = false;
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            // Handle window close
+            if (event.type == sf::Event::Closed)
                 window.close();
+
+            // Handle escape key press
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
+                window.close();
+
+            // Handle Dragging Event
+            if (event.type == sf::Event::MouseButtonPressed && sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+                float MouseX = float(sf::Mouse::getPosition(window).x);
+                float MouseY =  float(sf::Mouse::getPosition(window).y);
+
+                sf::Vector2f MousePos = {MouseX ,MouseY};
+                
+                //Check If we're actually on the circle
+                if(circle.getGlobalBounds().contains({MouseX,MouseY})){
+                    isDragging = true;
+                    dragOffset = circle.getPosition()- MousePos;
+                }
+            }
+
+            if(event.type == sf::Event::MouseButtonReleased ){
+
+                isDragging = false;
+            }
+
+            if(isDragging){
+
+                float MouseX = float(sf::Mouse::getPosition(window).x);
+                float MouseY =  float(sf::Mouse::getPosition(window).y);
+
+                sf::Vector2f MousePos = {MouseX ,MouseY};
+                circle.setPosition(MousePos + dragOffset);
             }
         }
-
         window.clear();
+        window.draw(sprite);
+        window.draw(circle);
         window.display();
     }
+    
+ 
 }
